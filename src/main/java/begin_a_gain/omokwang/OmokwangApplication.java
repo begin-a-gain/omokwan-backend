c14ba5a3d83f791996aa5fd08b7ef968c7b693d4
Jioo <>
2024-09-18 15:31:43 +0900

    feat: 개발환경 구축

---
+++ src/main/java/begin_a_gain/omokwang/OmokwangApplication.java
@@ -0,0 +1,12 @@
+package begin_a_gain.omokwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmokwangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmokwangApplication.class, args);
+    }
+}
---
+++ src/test/java/begin_a_gain/omokwang/OmokwangApplicationTests.java
@@ -0,0 +1,13 @@
+package begin_a_gain.omokwang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmokwangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
