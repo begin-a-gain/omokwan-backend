f8c518b6a6b6d4bdf66fcf63bf94f7c673be0832
Jioo <jiwoojung@qcells.com>
2024-09-29 22:43:43 +0900

    feat: add nickname valid test

--- src/test/java/begin_a_gain/omokwang/user/domain/UserServiceTest.java
+++ src/test/java/begin_a_gain/omokwang/user/domain/UserServiceTest.java
@@ -1,28 +1,57 @@
 package begin_a_gain.omokwang.user.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+import begin_a_gain.omokwang.user.repository.UserRepository;
+import begin_a_gain.omokwang.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class UserTest {
+@DisplayName("User 관련 테스트")
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
 
-    @ParameterizedTest(name = "winningNumber = {0}, userNumber = {1}, bonusNumber = {2}")
-    @CsvSource(value = {"1,2,3,4,5,6 : 1,2,3,4,5,6: 8"}, delimiter = ':')
-    @DisplayName("닉네임 중복 체크")
-    void nickname_duplicate_test(String winningNumbers, String userNumbers, int bonusNumber) {
+    @InjectMocks
+    private UserService userService;
 
-        assertThat(rank).isEqualTo(Rank.FIRST);
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("닉네임 중복 체크 - 닉네임이 존재하는 경우")
+    void testIsNicknameTaken_NicknameExists() {
+        String existingNickname = "existingNick";
+        when(userRepository.existsByNickname(existingNickname)).thenReturn(true);
+
+        boolean isTaken = userService.isNicknameTaken(existingNickname);
+
+        assertThat(isTaken).isTrue();
     }
 
-    @ParameterizedTest(name = "winningNumber = {0}, userNumber = {1}, bonusNumber = {2}")
-    @CsvSource(value = {"1,2,3,4,5,6 : 1,2,3,4,5,6: 8"}, delimiter = ':')
-    @DisplayName("닉네임 유효성 체크")
-    void nickname_validation_test(String winningNumbers, String userNumbers, int bonusNumber) {
-        Lotto winningLotto = Lotto.createFromString(winningNumbers);
-        Lotto userLotto = Lotto.createFromString(userNumbers);
-        boolean matchBonus = userNumbers.contains(String.valueOf(bonusNumber));
-        Rank rank = Rank.from(userLotto.matchCount(winningLotto), matchBonus);
+    @ParameterizedTest(name = "닉네임: {0}, 기대 결과: {1}")
+    @CsvSource({
+            "valid123, true",        // 유효한 닉네임
+            "가나다123, true",        // 한글과 숫자가 포함된 유효한 닉네임
+            "invalid!!, false",       // 특수문자가 포함된 닉네임
+            "a, false",               // 1글자 닉네임 (유효하지 않음)
+            "thisisaverylongname, false", // 10글자 초과 닉네임 (유효하지 않음)
+            "nick, true",            // 4글자로 유효한 닉네임
+            "닉네임test, true",       // 한글과 영문 조합으로 유효한 닉네임
+            "special@name, false"    // 특수문자 포함 (유효하지 않음)
+    })
+    @DisplayName("닉네임 유효성 검사 테스트")
+    void testIsValidNickname(String nickname, boolean expectedResult) {
+        boolean isValid = userService.isValidNickname(nickname);
 
-        assertThat(rank).isEqualTo(Rank.FIRST);
+        assertThat(isValid).isEqualTo(expectedResult);
     }
+
+
 }
