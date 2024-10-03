577c571a7c26c25f32e97488ce3c3c75ecbc8957
Jioo <jiwoojung@qcells.com>
2024-10-03 22:14:18 +0900

    fix: modify http method

--- src/main/java/begin_a_gain/omokwang/user/controller/UserController.java
+++ src/main/java/begin_a_gain/omokwang/user/controller/UserController.java
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +50,7 @@ public class UserController {
                     content = @Content(schema = @Schema(implementation = NicknameRequest.class))),
             @ApiResponse(responseCode = "400", description = "Invalid nickname or nickname already taken", content = @Content)
     })
-    @GetMapping("/validations/nicknames")
+    @PostMapping("/validations/nicknames")
     public ResponseEntity<String> checkNickname(@RequestBody NicknameRequest nicknameRequest) {
         Optional<String> validationError = userService.validateNickname(nicknameRequest.getNickname());
 
