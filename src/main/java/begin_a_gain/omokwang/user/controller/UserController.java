051c20fb8ad577cc6e1a16abeac8f6000a1ac13e
jioome <jioome17@gmail.com>
2025-06-29 22:47:05 +0900

    delete user

--- src/main/java/begin_a_gain/omokwang/user/controller/UserController.java
+++ src/main/java/begin_a_gain/omokwang/user/controller/UserController.java
@@ -11,6 +11,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,21 @@ public class UserController {
         final long userId = SecurityUtil.getCurrentUserSocialId();
         return userService.findBySocialId(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_USER));
+    }
+
+    @Operation(
+            summary = "Delete user account",
+            description = "Deletes the currently authenticated user's account based on their ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User account successfully deleted"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid or missing token"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteUser() {
+        long socialId = SecurityUtil.getCurrentUserSocialId();
+        userService.deleteUser(socialId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/begin_a_gain/omokwang/user/service/UserService.java
+++ src/main/java/begin_a_gain/omokwang/user/service/UserService.java
@@ -4,6 +4,7 @@ import begin_a_gain.omokwang.user.dto.User;
 import begin_a_gain.omokwang.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -45,4 +46,20 @@ public class UserService {
         return userRepository.findBySocialIdAndPlatform(socialId, platform);
     }
 
+    @Transactional
+    public void deleteUser(long socialId) {
+        var user = userRepository.findBySocialId(socialId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        
+        var maskedEmail = getMaskedEmail(user);
+        user.setEmail(maskedEmail);
+        user.setSocialId(null);
+        user.setDeleted(true);
+    }
+
+    private String getMaskedEmail(User user) {
+
+        var email = user.getEmail();
+        return "[DELETED]_" + UUID.randomUUID() + "_" + email;
+    }
 }
