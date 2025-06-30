39e34fe8828cd2ba1605ad5442e2a8580ce365a7
jioome <jioome17@gmail.com>
2025-06-30 23:43:47 +0900

    fix match participants

--- src/main/java/begin_a_gain/omokwang/match_detail/repository/MatchParticipantRepository.java
+++ src/main/java/begin_a_gain/omokwang/match_detail/repository/MatchParticipantRepository.java
@@ -2,8 +2,15 @@ package begin_a_gain.omokwang.match_detail.repository;
 
 
 import begin_a_gain.omokwang.match_detail.domain.MatchParticipant;
+import begin_a_gain.omokwang.user.dto.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MatchParticipantRepository extends JpaRepository<MatchParticipant, Long> {
     void deleteByUserId(Long userId);
+
+    @Query("select distinct mp.user from MatchParticipant mp where mp.match.id = :matchId")
+    List<User> findUsersByMatchId(@Param("matchId") Long matchId);
 }
